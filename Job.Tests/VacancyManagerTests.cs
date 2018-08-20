13de05ee06d8d89cb850d1d151869f1af3b60eb9
ruslan.nafisovich@gmail.com <ruslan.nafisovich@gmail.com>
2018-08-20 13:48:26 +0300

    Добавлен тест добавления новых вакансий вместо старых

--- Job.Tests/VacancyManagerTests.cs
+++ Job.Tests/VacancyManagerTests.cs
@@ -17,7 +17,7 @@ namespace Job.Tests
     public class VacancyManagerTests
     {
         [Test]
-        public async Task T()
+        public async Task ReturnsNewVacancies()
         {
             int count = 3;
 
@@ -38,6 +38,55 @@ namespace Job.Tests
             {
                 versionInfo.VersionInfoId = Guid.NewGuid();
             }).Returns(Task.FromResult(1));
+
+            var zpClientMock = new Mock<IZpClient>();
+            zpClientMock.Setup(t => t.GetVacancies(It.IsAny<int>(), It.IsAny<int>())).ReturnsAsync(new VacancyInfo
+            {
+                Count = 100 * count,
+                Vacancies = expectedVacancies
+            });
+
+            var vacancyManager = new VacancyManager(vacancyRepoMock.Object, versionRepoMock.Object, zpClientMock.Object);
+            await vacancyManager.UpdateVacancies();
+
+            Assert.AreEqual(expectedVacancies.Count() * count, actualVacancies.Count());
+        }
+
+        [Test]
+        public async Task ReturnsNewVacanciesInsteadOldVacancies()
+        {
+            int count = 3;
+
+            var expectedVacancies = new List<Vacancy>
+            {
+                new Vacancy(), new Vacancy()
+            };
+            var actualVacancies = new List<Vacancy>
+            {
+                new Vacancy(), new Vacancy()
+            };
+
+            var vacancyRepoMock = new Mock<IVacancyRepository>();
+            vacancyRepoMock.Setup(t => t.AddRange(It.IsAny<IEnumerable<Vacancy>>())).Callback((IEnumerable<Vacancy> vacancies) =>
+            {
+                actualVacancies.AddRange(vacancies);
+            }).Returns(Task.FromResult(1));
+            vacancyRepoMock.Setup(t => t.Remove(It.IsAny<Guid>())).Returns((Guid id) =>
+            {
+                actualVacancies.RemoveRange(0, 2);
+                return Task.FromResult(1);
+            });
+
+            var vertionId = Guid.NewGuid();
+            var versionRepoMock = new Mock<IVersionRepository>();
+            versionRepoMock.Setup(t => t.Add(It.IsAny<VersionInfo>())).Callback((VersionInfo versionInfo) =>
+            {
+                versionInfo.VersionInfoId = vertionId;
+            }).Returns(Task.FromResult(1));
+            versionRepoMock.Setup(t => t.GetLast(It.IsAny<DataType>())).ReturnsAsync(new VersionInfo
+            {
+                IsDownloaded = true
+            });
 
             var zpClientMock = new Mock<IZpClient>();
             zpClientMock.Setup(t => t.GetVacancies(It.IsAny<int>(), It.IsAny<int>())).ReturnsAsync(new VacancyInfo
