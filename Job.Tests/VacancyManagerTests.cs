e5111b311a8e1e452493d3f8aa4b8ada5ec513c5
ruslan.nafisovich@gmail.com <ruslan.nafisovich@gmail.com>
2018-08-20 13:35:44 +0300

    Добавлен тест на добавление вакансии

--- Job.Tests/VacancyManagerTests.cs
+++ Job.Tests/VacancyManagerTests.cs
@@ -7,6 +7,7 @@ using Moq;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -18,27 +19,37 @@ namespace Job.Tests
         [Test]
         public async Task T()
         {
+            int count = 3;
+
+            var expectedVacancies = new List<Vacancy>
+            {
+                new Vacancy(), new Vacancy()
+            };
+            var actualVacancies = new List<Vacancy>();
+
             var vacancyRepoMock = new Mock<IVacancyRepository>();
+            vacancyRepoMock.Setup(t => t.AddRange(It.IsAny<IEnumerable<Vacancy>>())).Callback((IEnumerable<Vacancy> vacancies) =>
+            {
+                actualVacancies.AddRange(vacancies);
+            }).Returns(Task.FromResult(1));
+
             var versionRepoMock = new Mock<IVersionRepository>();
             versionRepoMock.Setup(t => t.Add(It.IsAny<VersionInfo>())).Callback((VersionInfo versionInfo) =>
             {
                 versionInfo.VersionInfoId = Guid.NewGuid();
             }).Returns(Task.FromResult(1));
-            //versionRepoMock.Setup(t => t.GetLast(It.IsAny<DataType>())).ReturnsAsync(new VersionInfo());
 
             var zpClientMock = new Mock<IZpClient>();
             zpClientMock.Setup(t => t.GetVacancies(It.IsAny<int>(), It.IsAny<int>())).ReturnsAsync(new VacancyInfo
             {
-                Count = 300,
-                Vacancies = new List<Vacancy>
-                {
-                    new Vacancy(),
-                    new Vacancy()
-                }
+                Count = 100 * count,
+                Vacancies = expectedVacancies
             });
 
             var vacancyManager = new VacancyManager(vacancyRepoMock.Object, versionRepoMock.Object, zpClientMock.Object);
             await vacancyManager.UpdateVacancies();
+
+            Assert.AreEqual(expectedVacancies.Count() * count, actualVacancies.Count());
         }
     }
 }
